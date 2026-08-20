073d330e84995c06ed623a0428664b765e747639
Siddhant Chasta <siddhantchasta4199@gmail.com>
2026-08-21 00:03:29 +0530

    trigger redeploy

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -8,31 +8,44 @@
  * @module
  */
 
+import type * as chats from "../chats.js";
+import type * as messages from "../messages.js";
+
 import type {
   ApiFromModules,
   FilterApi,
   FunctionReference,
 } from "convex/server";
-import type * as chats from "../chats.js";
-import type * as messages from "../messages.js";
+
+declare const fullApi: ApiFromModules<{
+  chats: typeof chats;
+  messages: typeof messages;
+}>;
 
 /**
- * A utility for referencing Convex functions in your app's API.
+ * A utility for referencing Convex functions in your app's public API.
  *
  * Usage:
  * ```js
  * const myFunctionReference = api.myModule.myFunction;
  * ```
  */
-declare const fullApi: ApiFromModules<{
-  chats: typeof chats;
-  messages: typeof messages;
-}>;
 export declare const api: FilterApi<
   typeof fullApi,
   FunctionReference<any, "public">
 >;
+
+/**
+ * A utility for referencing Convex functions in your app's internal API.
+ *
+ * Usage:
+ * ```js
+ * const myFunctionReference = internal.myModule.myFunction;
+ * ```
+ */
 export declare const internal: FilterApi<
   typeof fullApi,
   FunctionReference<any, "internal">
 >;
+
+export declare const components: {};
